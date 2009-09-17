56390e2210ac3f3b54fdcf8fa4ee897a20164824
rogerdpack <rogerpack2005@gmail.com>
2009-09-17 06:34:07 -0600

    use new style process startup in doze

--- ext/ae-shell/ae-shell.rb
+++ ext/ae-shell/ae-shell.rb
@@ -52,15 +52,18 @@ class Shell < ArcadiaExt
         Arcadia.console(self,'msg'=>"Running #{_filename}...", 'level'=>'debug') # info?
         start_time = Time.now
         @arcadia['pers']['run.file.last']=_filename if _event.persistent
-        _cmd_ = "#{@arcadia['conf']['shell.ruby']} -C'#{File.dirname(_filename)}' '#{_filename}'"
+        executable = @arcadia['conf']['shell.ruby']
+        executable = @arcadia['conf']['shell.rubyw'] if is_windows?
+        _cmd_ = "#{executable} -C'#{File.dirname(_filename)}' '#{_filename}'"
+        
         if is_windows?
+          # use win32-process gem to startup a child process [not sure if linux needs something like this, too]
           require 'win32/process'
           require 'ruby-wmi'
-          output_file_name = "out_#{@@next_number}.txt"
-          @@next_number += 1
-          # windows needs to run it through another cmd for some reason or it doesn't redirect right
-          _cmd_ = "cmd /c \"#{_cmd_} > #{output_file_name} 2>&1 \""
-          child = Process.create(:command_line => _cmd_)
+          output_file_name = "out_#{@@next_number += 1}_#{Process.pid}.txt"
+          output = File.open(output_file_name, 'wb')
+          child = Process.create :command_line => _cmd_,  :startup_info => {:stdout => output}
+          
           timer=nil
           procy = proc {
             still_alive = WMI::Win32_Process.find(:first, :conditions => {:ProcessId => child.process_id})
@@ -84,7 +87,7 @@ class Shell < ArcadiaExt
           _cmd_ = "|#{_cmd_} 2>&1"
           Thread.new {
             begin
-              open(_cmd_,"r"){|f|
+              open(_cmd_, "r"){|f|
                 _readed = f.read
                 Arcadia.console(self,'msg'=>_readed, 'level'=>'debug')
                 _event.add_result(self, 'output'=>_readed)
